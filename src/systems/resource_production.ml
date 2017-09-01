7fb76a76bf6a9f8313a500d0c4b68504933ed065
P. Hotz <p.hotz@posteo.net>
2017-09-01 19:23:11 +0200

    Clean up

--- src/systems/resource_production.ml
+++ src/systems/resource_production.ml
@@ -14,27 +14,26 @@ let gold_to_players gs player_ids amount : Gamestate.t =
 
 let transfer_gold time_passed tree gs id : Gamestate.t =
   try 
-    let open Component in
-    let open Component.Point_mass in
-    let pm = Gamestate.point_mass gs id in
-    let nearby_bases = Quadtree.within tree pm.position 3. in
-
-    ignore @@ Lwt_io.printf "%d bases in the vicinity of gold mine %d (%.1f|%.1f)\n"
-                            (Core.List.length nearby_bases)
-                            id
-                            pm.position.x
-                            pm.position.y;
-
-    let number_of_bases = Core.List.length nearby_bases in
-    
-    let amount_per_s_per_player = 0.1 in
-
-    let total_amount = amount_per_s_per_player *. (Core.Float.of_int number_of_bases) in
-
     let open Component.Resource in
-
     let f = function
       | Some { amount = current_amount } ->
+
+         let open Component in
+         let open Component.Point_mass in
+         let pm = Gamestate.point_mass gs id in
+         let nearby_bases = Quadtree.within tree pm.position 3. in
+
+         ignore @@ Lwt_io.printf "%d bases in the vicinity of gold mine %d (%.1f|%.1f)\n"
+                                 (Core.List.length nearby_bases)
+                                 id
+                                 pm.position.x
+                                 pm.position.y;
+
+         let number_of_bases = Core.List.length nearby_bases in
+         
+         let amount_per_s_per_player = 0.1 in
+
+         let total_amount = amount_per_s_per_player *. (Core.Float.of_int number_of_bases) in
 
          let total_amount = Core.min total_amount current_amount in
 
@@ -53,9 +52,7 @@ let transfer_gold time_passed tree gs id : Gamestate.t =
          let gs = gold_to_players gs player_ids per_player in
          
          Some { amount = current_amount -. total_amount }
-      | None ->
-         ignore @@ Lwt_io.printf "missing resource component\n";
-         None
+      | None -> assert false
     in
     
     Core.Hashtbl.change gs.resources id ~f;
