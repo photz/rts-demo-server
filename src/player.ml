8728b05b32359be4b4f983e23c4b6346ec6d5afb
P. Hotz <p.hotz@posteo.net>
2017-09-01 17:03:39 +0200

    Untangle the code responsible for transferring gold from gold mines to players' accounts

--- src/player.ml
+++ src/player.ml
@@ -5,6 +5,9 @@ type t = {
 
 let create name funds = { name = name; funds = funds }
 
+let change_balance { name; funds } amount = { name;
+                                              funds = funds +. amount }
+
 let serialize (player : t) =
   let open Yojson.Basic in
   `Assoc [("name", `String player.name);
--- src/systems/resource_production.ml
+++ src/systems/resource_production.ml
@@ -1,4 +1,72 @@
+(** Returns the gamestate in which the balance of player whose id
+    occurs in the list has been increased by `amount` *)
+let gold_to_players gs player_ids amount : Gamestate.t =
+  let bump_funds gs player_id =
+    let open Gamestate in
+    let f = function
+      | Some player -> Some (Player.change_balance player amount)
+      | _ -> None
+    in
+    Core.Hashtbl.change gs.players player_id ~f;
+    gs
+  in
+  Core.List.fold player_ids ~init:gs ~f:bump_funds
 
+let transfer_gold time_passed tree gs id : Gamestate.t =
+  try 
+    let open Component in
+    let open Component.Point_mass in
+    let pm = Gamestate.point_mass gs id in
+    let nearby_bases = Quadtree.within tree pm.position 3. in
+
+    ignore @@ Lwt_io.printf "%d bases in the vicinity of gold mine %d (%.1f|%.1f)\n"
+                            (Core.List.length nearby_bases)
+                            id
+                            pm.position.x
+                            pm.position.y;
+
+    let number_of_bases = Core.List.length nearby_bases in
+    
+    let amount_per_s_per_player = 0.1 in
+
+    let total_amount = amount_per_s_per_player *. (Core.Float.of_int number_of_bases) in
+
+    let open Component.Resource in
+
+    let f = function
+      | Some { amount = current_amount } ->
+
+         let total_amount = Core.min total_amount current_amount in
+
+         let per_player =
+           total_amount /. (Core.Float.of_int number_of_bases)
+         in
+
+         Lwt_io.printf "giving every player %.1f gold\n" per_player;
+
+         let get_player_id (base_id, _) = Gamestate.ownership gs base_id
+         in
+
+         let player_ids = Core.List.map nearby_bases ~f:get_player_id
+         in
+
+         let gs = gold_to_players gs player_ids per_player in
+         
+         Some { amount = current_amount -. total_amount }
+      | None ->
+         ignore @@ Lwt_io.printf "missing resource component\n";
+         None
+    in
+    
+    Core.Hashtbl.change gs.resources id ~f;
+
+    gs
+
+  with
+  | Not_found -> ignore @@ Lwt_io.printf "not found\n"; gs
+  | _ -> ignore @@ Lwt_io.printf "error\n"; gs
+
+  
 
 let run entity_templates gs (time_passed : float) =
   let tree = Quadtree.create {x=0.; y=0.} 32.0 in
@@ -16,72 +84,5 @@ let run entity_templates gs (time_passed : float) =
 
   let gold_mine_ids = Core.Hashtbl.keys gs.resources in
 
-  let f id =
-    try 
-      let open Component in
-      let open Component.Point_mass in
-      let pm = Gamestate.point_mass gs id in
-      let nearby_bases = Quadtree.within tree pm.position 1. in
+  Core.List.fold gold_mine_ids ~init:gs ~f:(transfer_gold time_passed tree)
 
-      ignore @@ Lwt_io.printf "%d bases in the vicinity of gold mine %d (%.1f|%.1f)\n"
-                              (Core.List.length nearby_bases)
-                              id
-                              pm.position.x
-                              pm.position.y;
-
-      let number_of_bases = Core.List.length nearby_bases in
-      
-      let amount_per_s_per_player = 0.1 in
-
-      let total_amount = amount_per_s_per_player *. (Core.Float.of_int number_of_bases) in
-
-      let open Component.Resource in
-
-      let f = function
-        | Some { amount = current_amount } ->
-           if current_amount < total_amount then (
-             Lwt_io.printf "gold mine %d is running out gold\n" id;
-             let per_player = current_amount /. (Core.Float.of_int number_of_bases) in
-             Lwt_io.printf "giving every player %.1f gold\n" per_player;
-             Some { amount = 0. }
-           ) else (
-             let per_player = amount_per_s_per_player *. time_passed in
-             Lwt_io.printf "giving every player %.1f gold\n" per_player;
-
-             let player_ids = Core.List.map nearby_bases ~f:(fun (base_id, _) ->
-                                                             Gamestate.ownership gs base_id)
-             in
-
-             let bump_funds player_id =
-               let open Player in
-               Core.Hashtbl.change gs.players player_id
-                           ~f:(function
-                          | Some player ->
-                             let funds = player.funds +. per_player in
-                             Some {name=player.name; funds }
-                          | None ->
-                             Lwt_io.printf "missing player\n";
-                             None
-                  )
-             in
-
-             Core.List.iter player_ids ~f:bump_funds;
-
-             Some { amount = current_amount -. total_amount }
-           )
-        | None ->
-           ignore @@ Lwt_io.printf "missing resource component\n";
-           None
-      in
-  
-      Core.Hashtbl.change gs.resources id ~f
-
-    with
-    | Not_found -> ignore @@ Lwt_io.printf "not found\n"
-    | _ -> ignore @@ Lwt_io.printf "error\n"
-
-  in
-
-  Core.List.iter gold_mine_ids ~f;
-
-  gs
